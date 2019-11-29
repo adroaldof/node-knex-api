d444fcc780f8d7e5e9e6c1289f7d49ea633a5cf5
Adroaldo de Andrade <adroaldof@gmail.com>
2019-11-29 20:09:36 -0300

    feat(server): add initial express configs

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+import { host, port } from "./config";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  const { connection, socket } = req;
+  const ip = connection.remoteAddress || socket.remoteAddress;
+  return res.send({ ok: true, fromIp: ip });
+});
+
+app.listen(port, () => console.info(`Server started at ${host}:${port}`));
