314a67f20da502807bd40cde1614fa4924688c60
Adroaldo de Andrade <adroaldof@gmail.com>
2019-11-30 00:55:12 -0300

    feat(server): move express to its own directory

---
+++ src/express/server.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  const { connection, socket } = req;
+  const ip = connection.remoteAddress || socket.remoteAddress;
+
+  return res.send({ ok: true, fromIp: ip });
+});
+
+export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-import express, { Request, Response } from 'express';
+import { app } from './express/server';
+import { host, port } from './config/constants';
 
-import { host, port } from './config';
-
-const app = express();
-
-app.use(express.json({ limit: '10kb' }));
-
-app.get('/', (req: Request, res: Response) => {
-  const { connection, socket } = req;
-  const ip = connection.remoteAddress || socket.remoteAddress;
-
-  return res.send({ ok: true, fromIp: ip });
-});
-
-app.listen(port, () => console.info(`Server started at ${host}:${port}`));
+app.listen(port, () => console.info(`Server started at ${host}${port}`));
