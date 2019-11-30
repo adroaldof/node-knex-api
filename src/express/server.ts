fd9de63447b4957b928ff57395714f0a4dc219e0
Adroaldo de Andrade <adroaldof@gmail.com>
2019-11-30 01:11:28 -0300

    feat(sec): put json max size limit

--- src/express/server.ts
+++ src/express/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+app.use(express.json({ limit: '10kb' }));
+
 app.get('/', (req: Request, res: Response) => {
   const { connection, socket } = req;
   const ip = connection.remoteAddress || socket.remoteAddress;
