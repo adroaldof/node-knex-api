5fbab6d9b18434ce91d980a2d68c23e111a0e6cc
Adroaldo de Andrade <adroaldof@gmail.com>
2019-11-30 12:46:21 -0300

    feat(server): include some express configs

--- src/express/server.ts
+++ src/express/server.ts
@@ -1,12 +1,17 @@
 import express, { Application, Request, Response } from 'express';
 import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
 
 import { rateLimit } from './middlewares';
 
 const app: Application = express();
 
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(compression());
+app.use(cors());
 
 app.get('/', rateLimit, (req: Request, res: Response) => {
   const { connection, socket } = req;
