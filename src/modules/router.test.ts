4ed6727dde8c931afc913af2ec28ff1af2490ba8
Adroaldo de Andrade <adroaldof@gmail.com>
2019-12-01 15:34:02 -0300

    feat(routing): add 404 fallback routing

--- src/modules/router.test.ts
+++ src/modules/router.test.ts
@@ -6,4 +6,10 @@ describe('router', () => {
     const { body }: Response = await request(app).get('/');
     expect(Object.keys(body)).toEqual(['ok']);
   });
+
+  it('should get /unknown', async () => {
+    const { body, status }: Response = await request(app).get('/unknown');
+    expect(status).toEqual(404);
+    expect(Object.keys(body)).toEqual(['key', 'message']);
+  });
 });
--- src/modules/router.ts
+++ src/modules/router.ts
@@ -5,6 +5,9 @@ import healthz from './core';
 const router = Router();
 
 router.use('/healthz', healthz);
+
+// Default routes
 router.get('/', (_, res: Response) => res.send({ ok: true }));
+router.get('*', (_, res: Response) => res.status(404).send({ key: 'not-found', message: 'not found' }));
 
 export default router;
